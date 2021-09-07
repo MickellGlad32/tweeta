a26749c3b6fddd3cea90d380e6230993689a0978
Mickell Gladness <mickellgladness@gmail.com>
2021-09-07 15:18:05 -0400

    react app added inside

--- routes/users.js
+++ routes/users.js
@@ -44,4 +44,50 @@ bcrypt.hash(req.body.password, 10)
 // respond with success/error
 });
 
+router.post('/login', async(req,res) => {
+  // check if username and password exist
+  db.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  .then(() => {
+    if (user){
+      res.status(400).jsonp({
+        error: 'username already in use'
+      })
+      return 
+    }
+  })
+  // find user by username
+  const user = await db.User.findOne({
+      where: {
+        username: req.body.username
+      }
+  })
+
+  if(!user){
+    res.status(400).json({
+      error: 'could not find user with that username'
+    })
+    return
+  }
+  // check password
+  const success = await bcrypt.compare(req.body.password, user.password)
+  
+  if(!success){
+    res.status(401).json({
+      error: 'incorrect password'
+    })
+    return
+  }
+
+  // login 
+  req.session.user = user
+  // respond with success/ error
+  res.json({
+    success: 'Successfully logged in'
+  })
+})
+
 module.exports = router;
