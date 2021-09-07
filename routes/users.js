fc436c1721e6db7495ec705899718132d1710959
Mickell Gladness <mickellgladness@gmail.com>
2021-09-07 12:39:32 -0400

    added nodemon

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models')
+const bcrypt = require('bcrypt')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function(req, res, next) {
+// take the username, password
+if(!req.body.username || !req.body.password){
+  res.status(400).json({
+    error: 'please include username and password'
+  })
+}
+// create a new user
+// check if the username is already taken
+db.User.findOne({
+  where: {
+    username: req.body.username
+  }
+})
+.then(() => {
+  if (user){
+    res.status(400).jsonp({
+      error: 'username already in use'
+    })
+    return 
+  }
+})
+// hash password
+bcrypt.hash(req.body.password, 10)
+.then((hash) => {
+  //  store in database
+  db.User.create({
+    username: req.body.username,
+    password: hash
+  })
+  .then((user) => {
+    res.status(201).json({
+      success: 'User created'
+    })
+  })
+})
+
+// respond with success/error
 });
 
 module.exports = router;
